e01e7285396c88429e093759de1fdab1a4f7292e
JOSU BENKO <josubenko@gmail.com>
2023-07-06 12:36:54 +0200

    Tutorial n4

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.firstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    //new commit
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
